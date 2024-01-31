90739f470e3c94439e635072ef08e634fd3c83d4
nathan pennington <nathantp@gmail.com>
2024-01-31 14:10:58 -0600

    working

--- factorial.js
+++ factorial.js
@@ -1,5 +1,24 @@
 function factorial(num) {
-  return 0;
+
+  if (num === 0 || num === 1){
+      return 1;
+  } else {
+
+    for (let i = num - 1; i > 1; i--){
+      num *= i;
+    }
+
+
+
+  }
+return num 
+
+  
+  
 }
+
+console.log(factorial(6))
+
+
 
 module.exports = factorial;
